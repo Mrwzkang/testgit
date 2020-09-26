3ac2178743c9189987154fa2aa7ca3c88fa440ed
wzhaokang <1294595178@qq.com>
2020-09-26 23:11:32 +0800

    第一练习提交到本地库

---
+++ src/main/java/com/wzk/test/Poject.java
@@ -0,0 +1,11 @@
+package com.wzk.test;
+
+/**
+ * @author WZhaokang
+ * @date 2020/9/26 23:08
+ */
+public class Poject {
+    public static void main(String[] args) {
+        System.out.println("I love past");
+    }
+}
